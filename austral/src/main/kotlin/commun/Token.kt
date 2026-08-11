1ec6987dcf46c055aae4bf0651328fae9d18dec0
LucioZampella <lzluciozampella@gmail.com>
2026-08-10 22:04:07 -0300

    fix: restauro Token.kt perdido en el merge con la rama parser

---
+++ austral/src/main/kotlin/commun/Token.kt
@@ -0,0 +1,31 @@
+package austral.src.main.kotlin.commun
+
+enum class TokenType {
+    LET,
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    LITERAL,
+    ASSIGNMENT,
+    EOF // end of file
+    ,
+    NUMBER_LITERAL,
+    STRING_LITERAL,
+    COLON,
+    EQUAL,
+    SEMICOLON,
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    LEFT_PAREN,
+    RIGHT_PAREN
+}
+
+
+data class Token(
+    val type: TokenType,
+    val value: String,
+    val start: Position,
+    val end: Position
+)
