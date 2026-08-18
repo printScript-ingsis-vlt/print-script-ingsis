8032e517e01c045bedf068fdcee96ad68b44c0e2
Lucio Zampella <162721696+LucioZampella@users.noreply.github.com>
2026-08-18 20:05:39 -0300

    Refactor Interpreter to remove Result handling

--- interpreter/src/main/kotlin/Interpreter.kt
+++ interpreter/src/main/kotlin/Interpreter.kt
@@ -1,77 +1,43 @@
-import common.src.main.kotlin.result.Result
-import recurses.Program
-import recurses.Stmt
-import common.src.main.kotlin.result.CompilerError
-import common.src.main.kotlin.result.SemanticError
-import recurses.BinaryExpression
-import recurses.Environment
-import recurses.Expr
-import recurses.Identifier
-import recurses.Node
-import recurses.NumberLiteral
-import recurses.StringLiteral
-import recurses.Variable
-import recurses.VariableDeclaration
+package austral.src.main.kotlin.interpreter
 
+import austral.src.main.kotlin.commun.*
 
-class Interpreter(private val output: Output) { // --> El output es el destino
+class Interpreter(private val output: Output) {
+
     private val environment = Environment()
 
-    fun run(program: Program): Result<Unit, List<CompilerError>> =
-        try {
-            program.statements.forEach(::execute)
-            Result.Success(Unit)
-        } catch (e: InterpreterException) {
-            Result.Failure(listOf(e.error))
-        } // --> Interpreta el arbol recorriendolo statement a statement, y ejecutando
+    fun run(program: Program) {
+        program.statements.forEach(::execute)
+    }
 
     private fun execute(stmt: Stmt) = when (stmt) {
         is VariableDeclaration -> executeDeclaration(stmt)
-    } // --> Si es el statemente es un VD, ejecuta algo
+    }
 
     private fun executeDeclaration(stmt: VariableDeclaration) {
-        if (stmt.type != "number" && stmt.type != "string")
-            throw error(stmt, "Invalid type '${stmt.type}'")
         val value = stmt.value?.let(::evaluate)
-        if (value != null && value.type != stmt.type)
-            throw error(stmt, "Cannot assign a value of type '${value.type}' to a variable of type '${stmt.type}'")
         environment.declare(stmt.name, Variable(stmt.type, value))
-    } // -->  Si no es valido tiro error, evaluo si hay valor y si coinciden los tipos
+    }
 
-    private fun evaluate(expr: Expr): Value = when (expr) {
+    fun evaluate(expr: Expr): Value = when (expr) {
         is NumberLiteral -> NumberValue(expr.value)
         is StringLiteral -> StringValue(expr.value)
-        is Identifier -> { // --> "x" por ejemplo
-            val variable = environment.lookup(expr.name)
-                ?: throw error(expr, "recurses.Variable '${expr.name}' is not declared")
-            variable.value ?: throw error(expr, "recurses.Variable '${expr.name}' is not initialized")
-        } // --> Chequeo si se definio la variable y si se le asigno su respectivo valor
+        is Identifier -> environment.lookup(expr.name)!!.value!!
         is BinaryExpression -> {
             val left = evaluate(expr.left)
             val right = evaluate(expr.right)
             when {
                 expr.operator == "+" && (left is StringValue || right is StringValue) ->
-                    StringValue(left.asString() + right.asString()) // --> Suma de strings
-                expr.operator !in setOf("+", "-", "*", "/") ->
-                    throw error(expr, "Unknown operator '${expr.operator}'") // --> Operador valido
-                left !is NumberValue || right !is NumberValue ->
-                    throw error(expr, "Operator '${expr.operator}' requires numeric operands") // --> Suma de tipos valida
-                expr.operator == "/" && right.value == 0.0 ->
-                    throw error(expr, "Division by zero")
+                    StringValue(left.asString() + right.asString())
                 else -> NumberValue(
                     when (expr.operator) {
-                        "+" -> left.value + right.value
-                        "-" -> left.value - right.value
-                        "*" -> left.value * right.value
-                        else -> left.value / right.value
+                        "+" -> (left as NumberValue).value + (right as NumberValue).value
+                        "-" -> (left as NumberValue).value - (right as NumberValue).value
+                        "*" -> (left as NumberValue).value * (right as NumberValue).value
+                        else -> (left as NumberValue).value / (right as NumberValue).value
                     }
                 )
             }
         }
     }
-
-    private class InterpreterException(val error: SemanticError) : RuntimeException()
-
-    private fun error(node: Node, message: String): InterpreterException =
-        InterpreterException(SemanticError(node.position, message))
 }
