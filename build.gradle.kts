dff930925a6f51ea75a3142a212a500c77692442
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2026-08-25 19:51:27 -0300

    agregando los hooks al proyecto

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,28 @@ subprojects {
         mavenCentral()
     }
 }
+
+val installHooks by tasks.registering(Copy::class) {
+    description = "Copia los git hooks de .githooks a .git/hooks"
+    group = "git hooks"
+
+    from(file("$rootDir/.githooks"))
+    into(file("$rootDir/.git/hooks"))
+
+    // Asigna permisos de ejecución (rwxr-xr-x)
+    filePermissions {
+        user {
+            read = true
+            write = true
+            execute = true
+        }
+        other {
+            read = true
+            execute = true
+        }
+        group {
+            read = true
+            execute = true
+        }
+    }
+}
