1fe9c8066cc47aafd0611f2dda08dbfc28fc0c7d
LucioZampella <lzluciozampella@gmail.com>
2026-09-08 14:52:56 -0300

    feat: TokenMatcher definido, cuyas implementaciones matchear el caracter con un token en el caso de poder hacerlo

--- lexer/src/main/kotlin/TokenMatcher.kt
+++ lexer/src/main/kotlin/TokenMatcher.kt
@@ -1,2 +1,15 @@
+import recurses.Position
+import recurses.Token
+import result.LexicalError
+import result.Result
+
 interface TokenMatcher {
+    val name: String
+
+    fun canStartWith(character: Char): Boolean // --> Responde al lexer si puede leer ese caracter
+
+    fun match( // --> Consume todo lo perteneciente al token
+        cursor: LexerCursor,
+        start: Position,
+    ): Result<Token, LexicalError>
 }
---
+++ lexer/src/main/kotlin/matcherImplementations/IdentifierMatcher.kt
@@ -0,0 +1,5 @@
+package matcherImplementations
+
+class IdentifierMatcher {
+
+}
