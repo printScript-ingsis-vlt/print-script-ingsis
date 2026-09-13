f01f074c93899c9f0e78baa65b752f7994ea819a
LucioZampella <lzluciozampella@gmail.com>
2026-09-12 22:31:11 -0300

    feat: Creadas las versiones 1.0 y 1.1 en LexerConfigurations con los tokens correspondientes

--- lexer/src/main/kotlin/lexer/LexerConfigurations.kt
+++ lexer/src/main/kotlin/lexer/LexerConfigurations.kt
@@ -3,23 +3,53 @@ package lexer
 import token.TokenType
 
 object LexerConfigurations {
-    val default =
+    // let es la unica keyword que esta presente en todas las versiones
+    private val baseKeywords =
+        mapOf(
+            "let" to TokenType.LET,
+        )
+
+    // Operaciones que se usan en todas las versiones
+    private val baseOperators =
+        mapOf(
+            ":" to TokenType.COLON,
+            "=" to TokenType.EQUAL,
+            ";" to TokenType.SEMICOLON,
+            "+" to TokenType.PLUS,
+            "-" to TokenType.MINUS,
+            "*" to TokenType.STAR,
+            "/" to TokenType.SLASH,
+            "(" to TokenType.LEFT_PAREN,
+            ")" to TokenType.RIGHT_PAREN,
+        )
+
+    val v1_0 =
+        LexerConfiguration(
+            keywords = baseKeywords,
+            operators = baseOperators,
+        )
+
+    val v1_1 =
         LexerConfiguration(
             keywords =
-                mapOf(
-                    "let" to TokenType.LET,
-                ),
+                baseKeywords +
+                    mapOf(
+                        "const" to TokenType.CONST,
+                        "boolean" to TokenType.BOOLEAN,
+                        "if" to TokenType.IF,
+                        "else" to TokenType.ELSE,
+                        "true" to TokenType.TRUE,
+                        "false" to TokenType.FALSE,
+                        "readInput" to TokenType.READINPUT,
+                        "readEnv" to TokenType.READENV,
+                    ),
             operators =
-                mapOf(
-                    ":" to TokenType.COLON,
-                    "=" to TokenType.EQUAL,
-                    ";" to TokenType.SEMICOLON,
-                    "+" to TokenType.PLUS,
-                    "-" to TokenType.MINUS,
-                    "*" to TokenType.STAR,
-                    "/" to TokenType.SLASH,
-                    "(" to TokenType.LEFT_PAREN,
-                    ")" to TokenType.RIGHT_PAREN,
-                ),
+                baseOperators +
+                    mapOf(
+                        "{" to TokenType.LEFT_BRACE,
+                        "}" to TokenType.RIGHT_BRACE,
+                    ),
         )
+
+    val default = v1_0
 }
